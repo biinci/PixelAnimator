65bedbd0c492f7770e9d91cada9c573d87797f56
bilalinci <bilalincid7@gmail.com>
2025-07-27 13:22:24 +0300

    Remove typo in CanvasWindow.ResourceLoader

--- Editor/Windows/Canvas/CanvasWindow.ResourceLoader.cs
+++ Editor/Windows/Canvas/CanvasWindow.ResourceLoader.cs
@@ -5,7 +5,7 @@ namespace binc.PixelAnimator.Editor.Windows
     public partial class CanvasWindow : Window
     {
         private Texture2D cachedGridTexture;
-        private Vector2 lastGridSize;s
+        private Vector2 lastGridSize;
         private Color lastBlackColor;
         private Color lastWhiteColor;
         
